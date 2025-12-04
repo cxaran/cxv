506877f9894693bb295bee370e94f2503f6c8f9d
cxaran <45082311+cxaran@users.noreply.github.com>
2025-12-04 16:51:49 -0600

    Update Supabase environment variable names

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const axios = require('axios')
 // Supabase (Railway ready)
 // ========================
 
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     console.error('❌ Faltan variables de entorno SUPABASE_URL o SUPABASE_ANON_KEY')
@@ -215,3 +215,5 @@ builder.defineStreamHandler(async function (args) {
 
 const PORT = process.env.PORT || 7000
 serveHTTP(builder.getInterface(), { port: PORT })
+
+
