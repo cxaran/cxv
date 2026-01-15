b75b4b544515040e7aef35e0a652eac3a357a0f1
cxaran <45082311+cxaran@users.noreply.github.com>
2026-01-15 14:59:12 -0600

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,8 +1,31 @@
 const addonInterface = require('../server')
+const getRouter = require('stremio-addon-sdk/src/getRouter')
+
+const router = getRouter(addonInterface)
 
 module.exports = (req, res) => {
     if (!req.url) {
         req.url = '/'
     }
-    return addonInterface(req, res)
+
+    if (req.url === '/') {
+        res.statusCode = 302
+        res.setHeader('Location', '/manifest.json')
+        res.end()
+        return
+    }
+
+    router(req, res, (err) => {
+        if (err) {
+            console.error(err)
+            res.statusCode = 500
+            res.setHeader('Content-Type', 'application/json; charset=utf-8')
+            res.end(JSON.stringify({ err: 'handler error' }))
+            return
+        }
+
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json; charset=utf-8')
+        res.end(JSON.stringify({ err: 'not found' }))
+    })
 }
